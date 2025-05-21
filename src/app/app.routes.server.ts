13a50e709649388df296f1dc18b6a255b054389e
maearon <manhng132@gmail.com>
2025-05-21 17:29:42 +0700

    Configure SSR serverRoutes and disable prerender for dynamic paths

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,38 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'signup',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'users',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'account_activations/new',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'password_resets/new',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
