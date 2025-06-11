6f73715df01d7cf1265a087ae2d3481eb5311120
mark <manhng132@gmail.com>
2025-06-11 09:10:29 +0700

    Update gravatar.util.ts

--- src/app/utils/gravatar.util.ts
+++ src/app/utils/gravatar.util.ts
@@ -1,6 +1,7 @@
 import { Md5 } from 'ts-md5';
 
 export function gravatarUrl(email: string, size = 80): string {
-  const hash = Md5.hashStr(email.trim().toLowerCase());
+  const validEmail = email?.trim() || 'manhng132@gmail.com';
+  const hash = Md5.hashStr(validEmail.toLowerCase());
   return `https://secure.gravatar.com/avatar/${hash}?s=${size}`;
 }
